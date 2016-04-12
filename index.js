d87b0fbe447b7c1ce2f8e98cb9caf2a68ce92ee2
kostuyn <kostuyn@gmail.com>
2016-04-12 20:56:24 +0300

    token-generator

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ require('./services/server')('localhost', port);
 require('./services/client')('localhost', seedPort);
 require('./services/client')('localhost', seedPort);
 
+var tokenGenerator = require('./lib/token-generator');
+
+tokenGenerator.create(10, function (err, tokens) {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log(tokens);
+});
+
+
+
 
